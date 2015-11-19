1f0fd2a9d75d320c33c8d1769d8eabb2912fba8f
Joseph G <geis28@gmail.com>
2015-11-19 12:24:28 -0800

    Create library for this program

---
+++ libhosts.rb
@@ -0,0 +1,11 @@
+module WebFilter
+  HOSTS = /etc/hosts
+  hostsFile = File.read(HOSTS)
+  def WebFilter.approve (hostname, ip)
+    File.write(HOSTS, "#{hostsFile}\n#{ip} #{hostname}")
+  end
+  
+  def WebFilter.deny (hostname)
+    File.write(HOSTS, "#{hostsFile}\n0.0.0.0#{hostname}")
+  end
+end
